4601f56ee930f3a5f6da461faf341eaa04f94539
PleasantPineapple27 <nicholasguillen115@gmail.com>
2022-10-25 14:08:30 -0400

    commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,21 +10,21 @@ public class PlayerScript : MonoBehaviour
     public Text score;
     private int scoreValue = 0;
     public GameObject winTextObject;
-    private int 
+    public Text lives;
+    private int livesValue = 3;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public AudioSource musicSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        lives.text = livesValue.ToString();
         winTextObject.SetActive(false);
-    }
-    void SetWinText()
-    {
-        if(scoreValue >= 4)
-        {
-            winTextObject.SetActive(true);
-        }
+        musicSource.clip = musicClipOne;
+        musicSource.loop = true;
     }
 
     // Update is called once per frame
@@ -34,6 +34,8 @@ public class PlayerScript : MonoBehaviour
         float verMovement = Input.GetAxis("Vertical");
 
         rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
+        SetTeleport ();
+        SetWinText ();
     }
      private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -41,6 +43,13 @@ public class PlayerScript : MonoBehaviour
         {
             scoreValue += 1;
             score.text = scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+
+        if(collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            lives.text = "Lives: " + livesValue.ToString();
             Destroy(collision.collider.gameObject);
         }
 
@@ -56,4 +65,31 @@ public class PlayerScript : MonoBehaviour
             }
         }
     }
+
+    void SetTeleport()
+    {
+        if(scoreValue == 4)
+        {
+            transform.position = new Vector2(47.0f, 1.0f);
+            livesValue = 3;
+        }
+    }
+
+    void SetWinText()
+    {
+        lives.text = "Lives: " + livesValue.ToString();
+        if(livesValue <= 0)
+        {
+            Destroy(this);
+            //winTextObject = "You lose!";
+            winTextObject.SetActive(true);
+        }
+
+        if(scoreValue >= 8)
+        {
+            winTextObject.SetActive(true);
+        }
+
+    }
+
 }
